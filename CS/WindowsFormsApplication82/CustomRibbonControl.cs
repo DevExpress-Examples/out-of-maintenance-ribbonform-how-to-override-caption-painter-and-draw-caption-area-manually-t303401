d8704f34c0f80a3992a1343fbbcfca36e4eca135
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication82/CustomRibbonControl.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraBars.Ribbon.ViewInfo;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication82 {
+    public class CustomRibbonControl : RibbonControl {
+        private Color _captionColor;
+
+        public CustomRibbonControl() : base() {
+
+        }
+        protected override RibbonViewInfo CreateViewInfo() {
+            return new CustomRibbonViewInfo(this);
+        }
+        public Color CaptionColor { get { return _captionColor; } set { _captionColor = value; } }
+    }
+}
